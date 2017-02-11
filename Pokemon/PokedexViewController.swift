41b9825635917ef476d9a918960aede3cd4ab163
ziad adra <ziadadra@gmail.com>
2017-02-11 11:39:13 +0200

    11-2-2017 ok before push

---
+++ Pokemon/PokedexViewController.swift
@@ -0,0 +1,78 @@
+//
+//  PokedexViewController.swift
+//  Pokemon
+//
+//  Created by ziad adra on 1/30/17.
+//  Copyright © 2017 ziad adra. All rights reserved. Testing git hub
+//
+// still testing
+
+import UIKit
+
+class PokedexViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+     var caughtPokemons : [Pokemon] = []
+     var uncaughtPokemons : [Pokemon] = []
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         caughtPokemons = getAllcaughtPokemons()
+        uncaughtPokemons = getalluncaughtPokemons()
+        
+
+        // Do any additional setup after loading the view.
+       tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+        
+    }
+ 
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "CAUGHT"
+            
+        } else {
+            return "UNCAUGHT"
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return caughtPokemons.count
+            
+        } else {
+            return uncaughtPokemons.count
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let pokemon : Pokemon
+        if indexPath.section == 0 {
+            
+           
+              pokemon = caughtPokemons[indexPath.row]
+        } else {
+                pokemon = uncaughtPokemons[indexPath.row]
+        }
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemon.name
+        cell.imageView?.image = UIImage(named: pokemon.imageName!)
+        
+        return cell
+        
+    }
+    @IBAction func mapTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+
+}
---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by ziad adra on 1/29/17.
+//  Copyright © 2017 ziad adra. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var updateCount = 0
+    var pokemons : [Pokemon] = []
+    
+    
+    var manager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pokemons = getAllpokemon()
+       
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go")
+            mapView.delegate = self
+            
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+                
+                //spawn a pokemon
+                if let coord = self.manager.location?.coordinate {
+                    let anno = MKPointAnnotation()
+                    anno.coordinate = coord
+                    let randoLat = (Double(arc4random_uniform(200))-100)/70000.0
+                    let randoLon = (Double(arc4random_uniform(200))-100)/70000.0
+
+                    anno.coordinate.latitude += randoLat
+                    anno.coordinate.longitude += randoLon
+                    
+                    
+                    self.mapView.addAnnotation(anno)
+                    
+                }
+            })
+            
+            
+        } else {
+            
+            manager.requestWhenInUseAuthorization()
+        }
+        
+    }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            annoView.image = UIImage(named: "iss1")
+            var frame = annoView.frame
+            
+            frame.size.height=50
+            frame.size.width=50
+            annoView.frame = frame
+            
+            return annoView
+        }
+        let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        annoView.image = UIImage(named: "mew")
+        var frame = annoView.frame
+        
+        frame.size.height=50
+        frame.size.width=50
+        annoView.frame = frame
+        
+        return annoView
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 200, 200)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+            
+            
+            
+        } else {
+            manager.stopUpdatingLocation()
+        }
+        
+        
+    }
+    
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        
+        if let coord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coord, 200, 200)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+}
+
